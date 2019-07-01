cdf0731d7b64a492cc3d31e99935aa6e8685b58a
Richard Pos <richardnottoknow@gmail.com>
2019-07-01 15:06:06 +0200

    Section 3 is finished

--- components/src/old/index.1.41.js
+++ components/src/old/index.1.41.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from "./ApprovalCard";
+import CommentDetail from '../CommentDetail';
+import ApprovalCard from "../ApprovalCard";
 
 const App = () => {
 
