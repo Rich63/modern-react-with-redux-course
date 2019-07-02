33a6445f47cd8f9fbdb2e9c342286d0df84caf2d
Richard Pos <richardnottoknow@gmail.com>
2019-07-02 13:54:57 +0200

    until 4.55 class component with state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => console.log(position),
+//     err => console.log(err)
+//   );
 
-  return (
-    <div>Latitude: {position.Latitude}</div>
-  );
-};
+//   return (
+//     <div>Latitude: {position.Latitude}</div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state 
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+    position => {
+      this.setState({ lat: position.coords.latitude });
+      //NEVER, NEVER,NEVER,
+      //NEVER DO SOMETHING LIKE: this.state.lat = position.coords.latitude
+    },
+    err => console.log(err)
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div>Latitude: { this.state.lat } </div>
+  }
+}
 
 ReactDOM.render(
   <App />, document.querySelector('#root')
