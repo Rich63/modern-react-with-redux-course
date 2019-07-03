7b4a4e692fa50e1ea41cddef20dc451700bd3757
Richard Pos <richardnottoknow@gmail.com>
2019-07-03 12:12:04 +0200

    section-6-ends

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  // constructor(props) {  // Good place to do one-time setup
-  //   super(props);
-  //   this.state = { lat: null, errorMessage: '' };
-  // };
 
+  // This is the same as a constructor
   state = { lat: null, errorMessage: '' };
 
-  componentDidMount() { // Good place to do data-loading
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   };
 
-  componentDidUpdate() {  // Good place to do more data-loading when state/props change
-    console.log('My component was just updated - it rerendered');
-  };
-
-  componentWillUnmount() {  // Good place to do cleanup (especially for non-react stuff)
-    console.log('My component was just unmounted');
-  };
-
-  render() {  // avoid doing anything besides returning JSX
+  // A helper function to avoid conditionals in render()
+  renderContent() { 
     if (this.state.errorMessage && !this.state.lat) {
       return (
         <div>
@@ -34,15 +25,20 @@ class App extends React.Component {
       )
     };
 
+    // Here i get the state and send it as a prop to SeasonDisplay component
     if (!this.state.errorMessage && this.state.lat) {
-      return (
-        <div>
-          Latutude: { this.state.lat } 
-        </div>
-      )
+      return <SeasonDisplay lat={ this.state.lat } />
     };
 
-    return <div>loading.....</div>
+    return <Spinner message="Please accept location request." />
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{ this.renderContent() }</div>
+      </div>
+    );
   };
 };
 
