912b85f05eb36e02e147adc350aa932e5cfbdfff
Richard Pos <richardnottoknow@gmail.com>
2019-07-25 12:56:07 +0200

    Section 23 before use of Consumer

--- context/src/components/App.js
+++ context/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
   state = { language: 'english' }
@@ -8,6 +9,11 @@ class App extends React.Component {
     this.setState({ language });
   }
 
+  /*
+  First LanguageContext.Provider can change the language by clicking on a flag.
+  Second LanguageContext.Provider has a value of dutch which is not to be changed
+  third UserCreate uses the default value from LanguageContext which is english
+  */
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
@@ -16,7 +22,15 @@ class App extends React.Component {
           <i className="flag us" onClick={() => this.onLanguageChange('english')} />
           <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={ this.state.language }>
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        <LanguageContext.Provider value='dutch'>
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        <UserCreate />>
       </div>
     );
   };
